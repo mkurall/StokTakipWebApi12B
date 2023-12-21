351fe6a40ad3cebf22781a1777623cc558c6c425
Mustafa KURAL <mustafakural@outlook.com>
2023-12-21 09:57:56 +0300

    Kategori için Api CRUD metodları eklendi

--- Controllers/StokTakipController.cs
+++ Controllers/StokTakipController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using StokTakipWebApi12B.Models;
+using StokTakipWebApi12B.Protokol;
 
 namespace StokTakipWebApi12B.Controllers
 {
@@ -7,7 +8,7 @@ namespace StokTakipWebApi12B.Controllers
     [Route("api/v1/[action]")]
     public class StokTakipController : ControllerBase
     {
-       StokTakipDbContext ctx;
+        StokTakipDbContext ctx;
         public StokTakipController(StokTakipDbContext dbContext)
         {
             this.ctx = dbContext;
@@ -24,5 +25,74 @@ namespace StokTakipWebApi12B.Controllers
         {
             return ctx.TblKullanicilars.ToList();
         }
+
+        [HttpPost]
+        public ApiCevap KategoriEkle(TblKategoriler kategori)
+        {
+            ApiCevap cevap = new ApiCevap();
+
+            ctx.TblKategorilers.Add(kategori);
+            ctx.SaveChanges();
+            cevap.BasariliMi = true;
+            cevap.Data = kategori;
+
+            return cevap;
+        }
+
+        [HttpPost]
+        public ApiCevap KategoriListesiniGetir()
+        {
+            ApiCevap cevap = new ApiCevap();
+            cevap.BasariliMi = true;
+            cevap.Data = ctx.TblKategorilers.ToList();
+            return cevap;
+        }
+
+        [HttpPost]
+        public ApiCevap KategoriGuncelle(TblKategoriler kategori)
+        {
+            ApiCevap cevap = new ApiCevap();
+
+            var kat = ctx.TblKategorilers.FirstOrDefault(x=>x.KategaoriId == kategori.KategaoriId);
+
+            if(kat == null)
+            {
+                cevap.BasariliMi = false;
+                cevap.HataMesaji = "Böyle bir kayıt bulunumadı. kategoriId: " + kategori.KategaoriId;
+                return cevap;
+            }
+
+            cevap.BasariliMi = true;
+
+            kat.KategoriAdi = kategori.KategoriAdi;
+            ctx.SaveChanges();
+
+            cevap.Data = kat;
+
+            return cevap;
+        }
+
+        [HttpPost]
+        public ApiCevap KategoriSil(int id)
+        {
+            ApiCevap cevap = new ApiCevap();
+
+            var kat = ctx.TblKategorilers.FirstOrDefault(x => x.KategaoriId == id);
+
+            if(kat==null)
+            {
+                cevap.BasariliMi = false;
+                cevap.HataMesaji = "Böyle bir kayıt bulunumadı. kategoriId: " + id;
+                return cevap;
+            }
+
+            ctx.TblKategorilers.Remove(kat);
+            ctx.SaveChanges();
+            cevap.BasariliMi=true;
+            cevap.Data = true;
+
+            return cevap;
+        }
+
     }
 }
---
+++ Protokol/ApiCevap.cs
@@ -0,0 +1,10 @@
+﻿namespace StokTakipWebApi12B.Protokol
+{
+    public class ApiCevap
+    {
+        public bool BasariliMi { get; set; }
+        public string HataMesaji { get; set; }
+
+        public object Data { get; set; } 
+    }
+}
